092cefc07b815b5ec8ea722b347b4aef0135410b
tt4623 <sltebr33zy@gmail.com>
2021-02-04 22:29:00 -0600

    initial commit

---
+++ Algor-Snips/snippets.js
@@ -0,0 +1,8 @@
+// a function that takes in a string, and returns a string of numbers associated
+// with the alphabetic order, ONLY returns a number for letters
+const alphabetPosition = text => {
+  const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  return text.toLowerCase().replace(/[^A-Za-z0-9]/g, '').split('').map(el => {
+    return alpha.indexOf(el) + 1;
+  }).join(' ');
+}
